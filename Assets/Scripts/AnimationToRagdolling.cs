1434616cbcaec9bb2a02976b53936aae20e9cc96
mimbies <michelle.koops@haw-hamburg.de>
2025-04-08 17:04:18 +0200

    merge

---
+++ Assets/Scripts/AnimationToRagdolling.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationToRagdolling : MonoBehaviour
+{
+    public GameObject animatorToDisable;
+    public void disableAnimator()
+    {
+        animatorToDisable.GetComponent<Animator>().enabled = false;
+
+
+    }
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+
+}
